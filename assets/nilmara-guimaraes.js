ff063ea6c9dea67affb93542adcf47a87dfa53fa
revistasetari <revistasetari@gmail.com>
2026-09-07 16:08:52 -0300

    Corrigir somente a foto de Luciana usando SVG embutido e novo cache busting

--- assets/nilmara-guimaraes.js
+++ assets/nilmara-guimaraes.js
@@ -1,9 +1,9 @@
 (()=>{
-const BUILD='20260906-2055';
+const BUILD='20260907-1607';
 const PHOTOS={
   'Denise Stolle da Luz Weiss':`assets/denise-stolle-da-luz-weiss.jpg?v=${BUILD}`,
   'Luiz Fernando Ribas Monteiro':`assets/luiz-fernando-ribas-monteiro-v3.jpg?v=${BUILD}`,
-  'Luciana Maria Margoti':`assets/luciana-maria-margoti-v2.jpg?v=${BUILD}`,
+  'Luciana Maria Margoti':`assets/luciana-maria-margoti.svg?v=${BUILD}`,
   'Nilmara Almeida Guimarães':`assets/nilmara-almeida-guimaraes.jpg?v=${BUILD}`
 };
 
